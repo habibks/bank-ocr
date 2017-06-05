8221fa025b24b3062212be138fe6098edd8a73bd
habibks <ksentinhabib@gmail.com>
2017-06-05 19:01:02 +0200

    Recognize account number that contains only zeros: add test

---
+++ src/main/kotlin/net/habibksentini/bankocr/SevenSegmentAccountNumberEntry.kt
@@ -0,0 +1,9 @@
+package net.habibksentini.bankocr
+
+class SevenSegmentAccountNumberEntry(line1: String, line2: String, line3: String) {
+
+    fun getAccountNumber(): String {
+        throw UnsupportedOperationException("method not yet implemented")
+    }
+
+}
---
+++ src/test/kotlin/net/habibksentini/bankocr/SevenSegmentAccountNumberEntryTest.kt
@@ -0,0 +1,21 @@
+package net.habibksentini.bankocr
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.hamcrest.CoreMatchers.`is` as isEqualTo
+
+class SevenSegmentAccountNumberEntryTest{
+
+    @Test
+    fun given_sevenSegmentEntryContainOnlyZeros_then_recognizeAccountNumber(){
+        val line1 : String = " _  _  _  _  _  _  _  _  _ "
+        val line2 : String = "| || || || || || || || || |"
+        val line3 : String = "|_||_||_||_||_||_||_||_||_|"
+
+        val result = SevenSegmentAccountNumberEntry(line1,line2,line3).getAccountNumber()
+
+        assertThat(result, isEqualTo("000000000"))
+    }
+
+}
+
