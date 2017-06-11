bd84cb2f58adf50783b23c7680d69ce053babbb7
habibks <ksentinhabib@gmail.com>
2017-06-11 18:46:06 +0200

    Recognize account number that contains only ones: add impl

--- src/main/kotlin/net/habibksentini/bankocr/SevenSegmentAccountNumberEntry.kt
+++ src/main/kotlin/net/habibksentini/bankocr/SevenSegmentAccountNumberEntry.kt
@@ -1,9 +1,28 @@
 package net.habibksentini.bankocr
 
-class SevenSegmentAccountNumberEntry(line1: String, line2: String, line3: String) {
+class SevenSegmentAccountNumberEntry(val line1: String, val line2: String, val line3: String) {
 
     fun getAccountNumber(): String {
-      return "000000000"
+        val sevenSegmentAccountNumber = parse()
+        return sevenSegmentAccountNumber.map { SevenSegmentNumberEnum.getInt(it) }.joinToString("")
     }
 
+    fun parse(): List<String> {
+        val sevenSegmentAccountNumber: MutableList<String> = mutableListOf()
+        for (i in 0 until 27 step 3) {
+            val sevenSegmentNumber = parseSevenSegmentNumberLines(i)
+            sevenSegmentAccountNumber.add(sevenSegmentNumber)
+        }
+        return sevenSegmentAccountNumber
+    }
+
+    private fun parseSevenSegmentNumberLines(i: Int): String {
+        val startIndex = i
+        val endIndex = i.plus(3)
+        val sevenSegmentNumberFirstLineEntry = line1.substring(startIndex, endIndex)
+        val sevenSegmentNumberSecondLineEntry = line2.substring(startIndex, endIndex)
+        val sevenSegmentNumberThirdLineEntry = line3.substring(startIndex, endIndex)
+        val sevenSegmentNumber = "$sevenSegmentNumberFirstLineEntry$sevenSegmentNumberSecondLineEntry$sevenSegmentNumberThirdLineEntry"
+        return sevenSegmentNumber
+    }
 }
--- src/test/kotlin/net/habibksentini/bankocr/SevenSegmentAccountNumberEntryTest.kt
+++ src/test/kotlin/net/habibksentini/bankocr/SevenSegmentAccountNumberEntryTest.kt
@@ -1,13 +1,14 @@
 package net.habibksentini.bankocr
 
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.containsInAnyOrder
 import org.junit.Test
 import org.hamcrest.CoreMatchers.`is` as isEqualTo
 
 class SevenSegmentAccountNumberEntryTest{
 
     @Test
-    fun getAccountNumber_given_sevenSegmentEntryContainOnlyZeros_then_recognizeAccountNumber(){
+    fun getAccountNumber_given_sevenSegmentEntryContainsOnlyZeros_then_recognizeAccountNumber(){
         val line1 : String = " _  _  _  _  _  _  _  _  _ "
         val line2 : String = "| || || || || || || || || |"
         val line3 : String = "|_||_||_||_||_||_||_||_||_|"
@@ -18,7 +19,7 @@ class SevenSegmentAccountNumberEntryTest{
     }
 
     @Test
-    fun getAccountNumber_given_sevenSegmentEntryContainOnlyOnes_then_recognizeAccountNumber(){
+    fun getAccountNumber_given_sevenSegmentEntryContainsOnlyOnes_then_recognizeAccountNumber(){
         val line1 : String = "                           "
         val line2 : String = "  |  |  |  |  |  |  |  |  |"
         val line3 : String = "  |  |  |  |  |  |  |  |  |"
@@ -26,6 +27,25 @@ class SevenSegmentAccountNumberEntryTest{
         val result = SevenSegmentAccountNumberEntry(line1,line2,line3).getAccountNumber()
 
         assertThat(result, isEqualTo("111111111"))
+    }
+
+    @Test
+    fun parse_given_sevenSegmentEntry_then_return_sevenSegmentCodes(){
+        val line1 : String = "    _  _     _  _  _  _  _ "
+        val line2 : String = "  | _| _||_||_ |_   ||_||_|"
+        val line3 : String = "  ||_  _|  | _||_|  ||_| _|"
+
+        val result = SevenSegmentAccountNumberEntry(line1,line2,line3).parse()
+        assertThat(result, containsInAnyOrder(
+                "     |  |",
+                " _  _||_ ",
+                " _  _| _|",
+                "   |_|  |",
+                " _ |_  _|",
+                " _ |_ |_|",
+                " _   |  |",
+                " _ |_||_|",
+                " _ |_| _|"))
     }
 
 }
